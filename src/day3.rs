a43f7fd5bddc0f469c7d8d840a3ebbe4bd8434b0
Ravi Bhoraskar <>
2024-12-03 11:23:24 -0800

    Day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,13 +1,22 @@
 use anyhow::Result;
-use std::io::BufRead;
 use regex::Regex;
-
+use std::io;
+use std::io::Read;
 
 fn main() -> Result<()> {
-    let stdin = std::io::stdin();
-    let input = stdin.lock().lines().next().unwrap()?;
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
     let re = Regex::new(r"mul\([0-9]+,[0-9]+\)").unwrap();
-    let muls: Vec<&str> =  re.find_iter(&input).map(|m| m.as_str()).collect();
-    println!("{:?}", dates);
+    let sum = re
+        .find_iter(&input)
+        .map(|m| m.as_str())
+        .map(|m| {
+            let re = Regex::new(r"[0-9]+").unwrap();
+            re.find_iter(m)
+                .map(|m| m.as_str().parse::<i64>().unwrap() as i64)
+                .product::<i64>()
+        })
+        .sum::<i64>();
+    println!("{:?}", sum);
     Ok(())
 }
