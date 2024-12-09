9fdce3421bc7b5093e219d5a93581aee54b07737
Ravi Bhoraskar <>
2024-12-09 13:27:57 -0800

    rustfmt

--- src/day8.rs
+++ src/day8.rs
@@ -26,7 +26,7 @@ fn main() -> Result<()> {
             for j in 0..i {
                 let (x1, y1) = coords[i];
                 let (x2, y2) = coords[j];
-                if in_range(2 * x1 as i32- x2 as i32, 2 * y1 as i32 - y2 as i32 , &grid) {
+                if in_range(2 * x1 as i32 - x2 as i32, 2 * y1 as i32 - y2 as i32, &grid) {
                     newgrid[2 * x1 - x2][2 * y1 - y2] = '#';
                 }
                 if in_range(2 * x2 as i32 - x1 as i32, 2 * y2 as i32 - y1 as i32, &grid) {
